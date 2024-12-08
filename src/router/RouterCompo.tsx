2d5eff9aa12c3555ba759cd1e645cf1af7c78711
Jimit-GithubAcc <jimitnshah177@gmail.com>
2024-12-08 09:38:42 +0530

    clean up app file and setup routes.

--- src/router/RouterCompo.tsx
+++ src/router/RouterCompo.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
 import ProductDetails from "../pages/products/ProductDetails";
+import LoginPage from "../pages/authentication/login/LoginPage";
+import SignupPage from "../pages/authentication/signup/SignupPage";
+import ProfilePage from "../pages/profile/ProfilePage";
 
 const RouterCompo: React.FC = () => {
   return (
@@ -9,6 +17,10 @@ const RouterCompo: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<Navigate to={`/`} />} />
       </Routes>
     </Router>
   );
